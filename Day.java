883c6dfecc49e520eb0642b70029dd9316c3d0bd
sxia1 <sxia1@stuy.edu>
2018-06-01 08:27:39 -0400

    csv and main

---
+++ Day.java
@@ -0,0 +1,7 @@
+//Fermented Agave Juice
+//Sophia Xia, Johnny Wong, Clarence Cheng
+//APCS1 pd 1
+//05-29-18
+
+public class Day{
+}
---
+++ Driver.java
@@ -0,0 +1,15 @@
+//Fermented Agave Juice
+//Sophia Xia, Johnny Wong, Clarence Cheng
+//APCS1 pd 1
+//05-29-18
+
+public class Driver{
+    //Instance Variables
+
+    //Methods
+    
+    //Main Method
+    public static void main(String[] args){
+	
+    }
+}
